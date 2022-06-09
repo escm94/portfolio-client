d27dcd190eda5993325921be374cc15e511e24e6
escm94 <65869865+escm94@users.noreply.github.com>
2022-06-09 02:18:50 -0400

    fixing gaps between skills section and page edges to be more responsive

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -4,7 +4,7 @@ import {
   FaCss3,
   FaJs,
   FaReact,
-  FaGithub,
+  FaGitAlt,
   FaNodeJs,
 } from "react-icons/fa"
 import {
@@ -16,79 +16,142 @@ import {
   SiNetlify,
 } from "react-icons/si"
 
-const data = [
+const skillIcons = [
   {
     id: 1,
     icon: <FaHtml5 size="50px" title="HTML"></FaHtml5>,
-    type: "Frontend",
-    text: "HTML",
   },
   {
     id: 2,
     icon: <FaCss3 size="50px" title="CSS"></FaCss3>,
-    type: "Frontend",
-    text: "CSS",
   },
   {
     id: 3,
     icon: <FaJs size="50px" title="JavaScript"></FaJs>,
-    type: "Frontend",
-    text: "JavaScript",
   },
   {
     id: 4,
     icon: <SiTypescript size="50px" title="TypeScript"></SiTypescript>,
-    type: "Frontend",
-    text: "TypeScript",
   },
   {
     id: 5,
     icon: <FaReact size="50px" title="React"></FaReact>,
-    type: "Frontend",
-    text: "React",
   },
   {
     id: 6,
     icon: <SiTailwindcss size="50px" title="Tailwind CSS"></SiTailwindcss>,
-    type: "Frontend",
-    text: "Tailwind CSS",
   },
   {
     id: 7,
     icon: <FaNodeJs size="50px" title="Node"></FaNodeJs>,
-    type: "Backend",
-    text: "Node",
   },
   {
     id: 8,
     icon: <SiMongodb size="50px" title="MongoDB"></SiMongodb>,
-    type: "Backend",
-    text: "MongoDB",
   },
   {
     id: 9,
     icon: <SiDeno size="50px" title="Deno"></SiDeno>,
-    type: "Backend",
-    text: "Deno",
   },
   {
     id: 10,
-    icon: <FaGithub size="50px" title="GitHub"></FaGithub>,
-    type: "Others",
-    text: "GitHub",
+    icon: <FaGitAlt size="50px" title="GitHub"></FaGitAlt>,
   },
   {
     id: 11,
     icon: <SiNetlify size="50px" title="Netlify"></SiNetlify>,
-    type: "Others",
-    text: "Netlify",
   },
   {
     id: 12,
     icon: <SiVisualstudiocode size="50px" title="VS Code"></SiVisualstudiocode>,
-    type: "Others",
-    text: "VS Code",
   },
 ]
 
-export default data
+const skillListItemsBackend = [
+  {
+    id: 1,
+    text: "Node",
+  },
+  {
+    id: 2,
+    text: "Mongo",
+  },
+
+  {
+    id: 3,
+    text: "Deno",
+  },
+  {
+    id: 4,
+    text: "GraphQL",
+  },
+  {
+    id: 5,
+    text: "SQL",
+  },
+  {
+    id: 6,
+    text: "REST API",
+  },
+]
+
+const skillListItemsFrontend = [
+  {
+    id: 1,
+    text: "HTML",
+  },
+  {
+    id: 2,
+    text: "CSS",
+  },
+  {
+    id: 3,
+    text: "Javascript",
+  },
+  {
+    id: 4,
+    text: "TypeScript",
+  },
+  {
+    id: 5,
+    text: "React",
+  },
+  {
+    id: 6,
+    text: "Tailwind CSS",
+  },
+]
+
+const skillListItemsOthers = [
+  {
+    id: 1,
+    text: "Git",
+  },
+  {
+    id: 2,
+    text: "Responsive Design",
+  },
+  {
+    id: 3,
+    text: "VS Code",
+  },
+  {
+    id: 4,
+    text: "SEO",
+  },
+  {
+    id: 5,
+    text: "Scrum",
+  },
+  {
+    id: 6,
+    text: "Netlify",
+  },
+]
+
+export {
+  skillIcons,
+  skillListItemsFrontend,
+  skillListItemsBackend,
+  skillListItemsOthers,
+}
